5ae569d48d87de1a7209766546cc8f0996259ecd
Asif Muhammed <asifasifpsps@gmail.com>
2023-02-11 13:22:25 +0530

    created 3 Schemas in model page(ChatModel,MessageModel,UserModel)

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const {chats} = require('./data/data.js');
+
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+
+
+app.get('/api/chat/:id', (req, res) => {
+    console.log(req.params.id);
+    const singleChat = chats.find((chat) => chat._id === req.params.id);
+    res.send(singleChat);
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server Started on PORT ${PORT} `))
