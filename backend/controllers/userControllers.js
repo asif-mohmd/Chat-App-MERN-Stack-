0123813d0274359b4b26b00c4d4369bd803fd2cb
Asif Muhammed <asifasifpsps@gmail.com>
2023-03-11 17:40:58 +0530

    postman search user completed

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -59,5 +59,23 @@ const authUser = asyncHandler(async (req,res) =>{
     }
 })
 
+// /api/user?search=piyush
 
-module.exports = {registerUser , authUser}
+const allUsers = asyncHandler(async (req,res) =>{
+    const keyword = req.query.search ? {
+        $or: [
+            {name: {$regex: req.query.search, $options: 'i'}},
+            {email: {$regex: req.query.search, $options: 'i'}}
+        ],
+    } 
+    : {};
+
+    const users = await User.find(keyword)
+    
+
+    res.send(users);
+}
+)
+
+
+module.exports = {registerUser , authUser ,allUsers}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { registerUser, authUser } = require('../controllers/userControllers.js');
+const { registerUser, authUser,allUsers } = require('../controllers/userControllers.js');
+
 
 const router = express.Router();
 
 
-router.route('/').post(registerUser);
+router.route("/").post(registerUser).get(allUsers);
 router.post('/login', authUser)
 
 
