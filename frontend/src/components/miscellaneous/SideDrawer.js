fd194431dbe5938873ed05a76af6499a2c2c4a2f
Asif Muhammed <asifasifpsps@gmail.com>
2023-03-19 19:28:15 +0530

    Header section completed and logout

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,6 +1,25 @@
 import React from "react";
-import { Box, Button, Text, Tooltip  } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+  DrawerHeader,
+} from "@chakra-ui/react";
+
 import { useState } from "react";
+import { BellIcon ,ChevronDownIcon } from "@chakra-ui/icons";
+import { ChatState } from "../../Context/ChatProvider";
+import ProfileModal from "./ProfileModal";
+import { useHistory } from "react-router-dom";
 
 function SideDrawer() {
   const [search, setSearch] = useState("");
@@ -8,9 +27,25 @@ function SideDrawer() {
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
 
+   const { user } = ChatState();
+   const history = useHistory();
+
+   const logoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    history.push("/");
+   }
+   
   return (
     <div>
-      <Box>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px5px 10px"
+        borderWidth="5px"
+      >
         <Tooltip label="Search Users to Chat" hasArrow placement="bottom-end">
           <Button variant="ghost">
             <i className="fas fa-search"></i>
@@ -19,7 +54,65 @@ function SideDrawer() {
             </Text>
           </Button>
         </Tooltip>
+        <Text fontSize="2xl" fontFamily="Work sans">
+          Talk-A-Tive
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize={"2xl"} m={"1"} />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                name={user.name}
+                cursor="pointer"
+                src={user.pic}
+              />
+            </MenuButton>
+
+            <MenuList>
+              <ProfileModal user={user}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModal>
+              <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
       </Box>
+
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
+          <DrawerBody>
+            <Box d="flex" pb={2}>
+              <Input
+                placeholder="Search by name or email"
+                mr={2}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+            </Box>
+            {loading ? (
+              <ChatLoading />
+            ) : (
+              searchResult?.map((user) => (
+                <UserListItem
+                  key={user._id}
+                  user={user}
+                  handleFunction={() => accessChat(user._id)}
+                />
+              ))
+            )}
+            {loadingChat && <Spinner ml="auto" d="flex" />}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -5,13 +5,13 @@ import MyChats from "../components/MyChats";
 import ChatBox from "../components/ChatBox";
 
   const ChatPage = () => {
-  const {user} = ChatState();
+  const { user } = ChatState();
 
   return (
     <div style={{ width: "100%" }}>
       {user && <SideDrawer/>}
       <Box
-      d='flex'
+      display='flex'
       justifyContent='space-between'
       w='100%'
       h="91.5vh"
