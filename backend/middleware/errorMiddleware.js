9cbd1a46569219880421a5c0f37bc781aea99e76
Asif Muhammed <asifasifpsps@gmail.com>
2023-07-22 20:00:36 +0530

    Chat app minor change 3

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -13,4 +13,4 @@ const errorHandler = (err, req, res, next) => {
     });
 }
 
-module.exports = {  notFound,errorHandler,};
+module.exports = { notFound, errorHandler, };
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const {chats} = require('./data/data.js');
+const { chats } = require('./data/data.js');
 const connectDB = require('./config/db.js');
 const colors = require('colors');
 const userRoutes = require('./routes/userRoutes.js');
 const chatRoutes = require('./routes/chatRoutes.js');
 const messageRoutes = require('./routes/messageRoutes.js');
-const {notFound, errorHandler} = require('./middleware/errorMiddleware.js');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
 
 dotenv.config();
 connectDB()
 const app = express();
 
-app.use(express.json() ) // to accept JSON Data
+app.use(express.json()) // to accept JSON Data
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.use('/api/user',userRoutes)
-app.use('/api/chat',chatRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/chat', chatRoutes)
 app.use('/api/message', messageRoutes)
 
 app.use(notFound)
@@ -33,42 +33,42 @@ const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
         origin: 'http://localhost:3000',
-        },
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('connected to socket.io');
+
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        console.log(userData._id);
+        socket.emit("connected");
+
+    })
+
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User Joined room" + room);
+    });
+
+    socket.on("typing", (room) => {
+        socket.in(room).emit("typing");
+    });
+
+    socket.on("stop typing", (room) => {
+        socket.in(room).emit("stop typing");
+    });
+
+    socket.on("new message", (newMessageRecieved) => {
+        var chat = newMessageRecieved.chat;
+        if (!chat.users)
+            return console.log('Chat.users not defined');
+
+        chat.users.forEach((user) => {
+            if (user._id == newMessageRecieved.sender._id)
+                return;
+            socket.in(user._id).emit("message recieved", newMessageRecieved);
         });
 
-        io.on('connection', (socket) => {
-            console.log('connected to socket.io'); 
-           
-            socket.on("setup" , (userData)=>{
-                socket.join(userData._id);
-                console.log(userData._id);
-                socket.emit("connected");
-
-            })
-
-            socket.on("join chat", (room) => {
-                socket.join(room);
-                console.log("User Joined room" + room);
-            });
-
-            socket.on("typing", (room) => {
-                socket.in(room).emit("typing");
-            });
-            
-            socket.on("stop typing", (room) => {
-                socket.in(room).emit("stop typing");
-            });
-
-            socket.on("new message", (newMessageRecieved) => {
-                var chat = newMessageRecieved.chat;
-                if(!chat.users)
-                return console.log('Chat.users not defined');
-
-                chat.users.forEach((user) => {
-                    if(user._id == newMessageRecieved.sender._id)
-                    return;
-                    socket.in(user._id).emit("message recieved", newMessageRecieved);
-                });
-                
-            });
-        });
+    });
+});
