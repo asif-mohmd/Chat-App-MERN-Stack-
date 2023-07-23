5ed912cd0c4cec03e773e91d3a7be5582bd45dde
Asif Muhammed <asifasifpsps@gmail.com>
2023-07-23 12:12:00 +0530

    Message model space removed

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-
-
 const messageModel = mongoose.Schema({
     sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     content: { type: String, trim: true },
