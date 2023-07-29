136adf104ea9dde7e4fc05fa0ce6f0830cabf62a
Asif Muhammed <asifasifpsps@gmail.com>
2023-07-29 13:51:08 +0530

    Login react page edits

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -6,39 +6,38 @@ import axios from 'axios'
 import { useHistory } from 'react-router-dom'
 
 function Login() {
-const [show, setShow] = useState(false)
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [loading, setLoading] = useState(false)
+  const [show, setShow] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
 
-    const toast = useToast()
-    const history = useHistory()
-    const handleClick = () => setShow(!show)
-  
-    const submitHandler = async() => {
-       setLoading(true)
-       if(email === '' || password === '') {
-         toast({
-           title: "Please fill all the fields!",
-           status: "warning",
-           duration: 5000,
-           isClosable: true,
-           position: 'bottom',
-         })
-         setLoading(false)
-         return;
-       }
+  const toast = useToast()
+  const history = useHistory()
+  const handleClick = () => setShow(!show)
 
-       try{
-        const config = {
-          headers: {
-            'Content-type': 'application/json'
+  const submitHandler = async () => {
+    setLoading(true)
+    if (email === '' || password === '') {
+      toast({
+        title: "Please fill all the fields!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      })
+      setLoading(false)
+      return;
+    }
 
-       }
+    try {
+      const config = {
+        headers: {
+          'Content-type': 'application/json'
 
-     };
+        }
+      };
 
-      const {data} = await axios.post('/api/user/login', {email, password}, config)
+      const { data } = await axios.post('/api/user/login', { email, password }, config)
       toast({
         title: "Logged In Successfully!",
         status: "success",
@@ -49,7 +48,7 @@ const [show, setShow] = useState(false)
       localStorage.setItem('userInfo', JSON.stringify(data))
       setLoading(false)
       history.push('/chats')
-    } catch(err) {
+    } catch (err) {
       toast({
         title: "Invalid Credentials!",
         description: err.response.data.message,
@@ -62,67 +61,62 @@ const [show, setShow] = useState(false)
     }
   }
 
-
-
-
-
-
   return (
-  <VStack spacing='5px' color='black'>
+    <VStack spacing='5px' color='black'>
 
-    <FormControl id='email' isRequired>
+      <FormControl id='email' isRequired>
         <FormLabel>Email</FormLabel>
-        <Input 
-         value={email}
-        placeholder='Enter Your Email'
-        onChange={(e) => setEmail(e.target.value)}
+        <Input
+          value={email}
+          placeholder='Enter Your Email'
+          onChange={(e) => setEmail(e.target.value)}
         />
-    </FormControl>
+      </FormControl>
 
-    <FormControl id='password' isRequired>
+      <FormControl id='password' isRequired>
         <FormLabel>Password</FormLabel>
         <InputGroup>
-        <Input
-        type={show? "text" : "password"}
-        placeholder='Enter Your Password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        />
-        |<InputRightElement width="4.5rem">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder='Enter Your Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          |<InputRightElement width="4.5rem">
             <Button h="1.75rem" size="sm" onClick={handleClick}>
               {show ? "Hide" : "Show"}
             </Button>
           </InputRightElement>
         </InputGroup>
-    </FormControl>
+      </FormControl>
 
-  
-    
-    <Button 
-    colorScheme='blue' 
-    width="100%" 
-    style={{marginTop :15 }} 
-    onClick={submitHandler} 
-    isLoading={loading}
-    >
-      Login
+
+
+      <Button
+        colorScheme='blue'
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={loading}
+      >
+        Login
       </Button>
 
-      <Button 
-      variant="solid"
-    colorScheme='red' 
-    width="100%" 
-    style={{marginTop :15 }} 
-    onClick={()=>{
-      setEmail('guest@example.com')
-      setPassword('123456')
-    }} 
-    >
-      Get Guest User Credentials
+      <Button
+        variant="solid"
+        colorScheme='red'
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={() => {
+          setEmail('guest@example.com')
+          setPassword('123456')
+        }}
+      >
+        Get Guest User Credentials
       </Button>
 
 
-  </VStack>
+    </VStack>
   )
 }
 
