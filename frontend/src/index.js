2bdb05a0d5e7e63fead293f7921f6522e3b91356
Asif Muhammed <asifasifpsps@gmail.com>
2023-07-29 13:55:50 +0530

    index edits

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom';
 import ChatProvider from './Context/ChatProvider';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <ChakraProvider>
      <BrowserRouter>
