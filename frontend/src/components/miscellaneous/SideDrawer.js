ec7c10a2e255af4bba441ff77f3b5d4bfc97e257
Asif Muhammed <asifasifpsps@gmail.com>
2023-08-08 21:15:45 +0530

    update

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -21,12 +21,12 @@ import {
 
 
 
-  
+
 
 } from "@chakra-ui/react";
 
 import { useState } from "react";
-import { BellIcon ,ChevronDownIcon } from "@chakra-ui/icons";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { ChatState } from "../../Context/ChatProvider";
 import ProfileModal from "./ProfileModal";
 import { useHistory } from "react-router-dom";
@@ -44,19 +44,19 @@ function SideDrawer() {
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
 
-   const { user , setSelectedChat , chats ,setChats , notification , setNotification } = ChatState();
-   const history = useHistory();
+  const { user, setSelectedChat, chats, setChats, notification, setNotification } = ChatState();
+  const history = useHistory();
 
-   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-   const logoutHandler = () => {
+  const logoutHandler = () => {
     localStorage.removeItem("userInfo");
     history.push("/");
-   }
+  }
 
-   const toast = useToast();
+  const toast = useToast();
 
-   const handleSearch = async () => {
+  const handleSearch = async () => {
     if (!search) {
       toast({
         title: "Please Enter something in search",
@@ -79,7 +79,7 @@ function SideDrawer() {
 
       const { data } = await axios.get(`/api/user?search=${search}`, config);
 
-     
+
 
       setLoading(false);
       setSearchResult(data);
@@ -94,37 +94,37 @@ function SideDrawer() {
         position: "bottom-left",
       });
     }
-    
+
   };
-const accessChat = async (userId) => {
-  console.log(userId);
+  const accessChat = async (userId) => {
+    console.log(userId);
 
-  try {
-    setLoadingChat(true);
-    const config = {
-      headers: {
-        "Content-type": "application/json",
-        Authorization: `Bearer ${user.token}`,
-      },
-    };
-    const { data } = await axios.post(`/api/chat`, { userId }, config);
+    try {
+      setLoadingChat(true);
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.post(`/api/chat`, { userId }, config);
 
-    if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
-    setSelectedChat(data);
-    setLoadingChat(false);
-    onClose();
-  } catch (error) {
-    toast({
-      title: "Error fetching the chat",
-      description: error.message,
-      status: "error",
-      duration: 5000,
-      isClosable: true,
-      position: "bottom-left",
-    });
-  }
-};
-   
+      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
+      setSelectedChat(data);
+      setLoadingChat(false);
+      onClose();
+    } catch (error) {
+      toast({
+        title: "Error fetching the chat",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+
   return (
     <div>
       <Box
@@ -150,21 +150,21 @@ const accessChat = async (userId) => {
         <div>
           <Menu>
             <MenuButton p={1}>
-              <NotificationBadge count={notification.length} 
-              effect={Effect.SCALE}
+              <NotificationBadge count={notification.length}
+                effect={Effect.SCALE}
               />
               <BellIcon fontSize={"2xl"} m={"1"} />
             </MenuButton>
             <MenuList pl={2} >
               {!notification.length && "No New Notification"}
               {notification.map((notif) => (
-                <MenuItem key={notif._id} onClick={()=>{
+                <MenuItem key={notif._id} onClick={() => {
                   setSelectedChat(notif.chat)
                   setNotification(notification.filter((n) => n !== notif))
                 }} >
                   {notif.chat.isGroupChat
-                   ? `New Message in ${notif.chat.chatName}` :
-                   `New Message From ${getSender(user,notif.chat.users)}`}
+                    ? `New Message in ${notif.chat.chatName}` :
+                    `New Message From ${getSender(user, notif.chat.users)}`}
                 </MenuItem>
               ))}
             </MenuList>
